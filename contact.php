a85f45b956eb1bf3fe7c7dd3f22886b696cad68d
5Y2V3573R <sylvestersiani@5y2v3573r.home>
2016-05-30 23:40:45 +0100

    contact
    setting up contact form page

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Get in touch</title>
+</head>
+<body>
+
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+		
+	</form>
+
+</body>
+</html>
