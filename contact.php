aedcdd6db97b08fe428313484c62a586f156ae90
5Y2V3573R <sylvestersiani@5y2v3573r.home>
2016-05-30 23:45:20 +0100

    front end of the contact page

--- contact.php
+++ contact.php
@@ -6,7 +6,18 @@
 <body>
 
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-		
+		<ul>
+			<li><input type="text" name="fname"></li>
+			<li><input type="text" name="lname"></li>
+			<li><input type="email" name="mail"></li>
+			<li><input type="number" name="number"></li>
+			<li><select>
+					<option>E-comerce</option>
+					<option>Basic</option>
+					<option>Blog</option>
+					<option>other</option>
+			</select></li>
+		</ul>
 	</form>
 
 </body>
