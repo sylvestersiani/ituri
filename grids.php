818f3fce360f9b8a545ff1ea09bfd94c6bf6d48c
crtndrmr <plopes.sw@gmail.com>
2016-05-31 18:50:46 +0100

    Responsive Grids

---
+++ grids.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<title>Untitled Document</title>
+<link rel="stylesheet" href="css/grids.css" type="text/css" media="screen">
+</head>
+
+<body>
+	<div class="section group">
+        <div class="grid_1">
+        1 of 6
+        </div>
+        <div class="grid_2">
+        1 of 6
+        </div>
+        <div class="grid_3">
+        1 of 6
+        </div>
+        <div class="grid_4">
+        1 of 6
+        </div>
+        <div class="grid_5">
+        1 of 6
+        </div>
+        <div class="grid_6">
+        1 of 6
+        </div>
+    </div>
+</body>
+</html>
