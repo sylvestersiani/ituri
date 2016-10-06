44ad3a3f37099ed97a21324d2701dd61d90d285f
crtndrmr <plopes.sw@gmail.com>
2016-10-06 14:50:59 +0100

    Pop Up Model

--- inc/template/success.php
+++ inc/template/success.php
@@ -1,10 +1,12 @@
 
-	<div id="success-outer">
-		<div id="success">
-
-				<h1>Success</h1>
-				<a href="index.php?home='gallery'">click to view some of our selected workk</a>
-
-		</div>
+	<div id="success-outer"></div>
+    <div id="success">
+    	<div class="content">
+			<h1>Success</h1>
+            <p>
+            	We'll get in contact with you as soon as possible. In the meanwhile take time to swift through some of our previous works.
+            </p>
+			<a href="index.php?home='gallery'">click to view some of our selected workk</a>
+			</div>
 	</div>
 
